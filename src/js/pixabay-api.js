8306108232540a1cb300a8b10a924d0477b32f3b
Bohdana <LipskaBogdana1990@gmail.com>
2024-08-05 16:32:22 +0200

    second commit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,17 @@
-import '../css/styles.css';
+export function fetchImages(query) {
+  const URL = 'https://pixabay.com/api/';
+  const API_KEY = '45282736-76f00277f987f73abb95d9f87';
+
+  return fetch(
+    `${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import '../src/css/styles.css';
+import { fetchImages } from './js/pixabay-api';
+import { renderImageCard, clearGallery } from './js/render-functions';
+
+const searchForm = document.querySelector('.search-form');
+const loader = document.querySelector('.loader');
+const input = document.querySelector('.input-text');
+
+searchForm.addEventListener('submit', handleSearch);
+
+function handleSearch(event) {
+  event.preventDefault();
+  clearGallery();
+  loader.classList.remove('hidden');
+
+  const searchWord = input.value.trim().toLowerCase();
+  fetchImages('${searchWord}').then(data => {
+    if (data.total === 0 || searchWord === '') {
+      iziToast.error({
+        position: 'topRight',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      loader.classList.add('hidden');
+      return;
+    } else {
+      renderImageCard(data);
+    }
+    loader.classList.add('hidden');
+  });
+  searchForm.reset();
+}
